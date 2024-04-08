76c3b592e9e2e34357f256b2049a7310423bf5f4
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-04-08 12:35:52 -0400

    Add files via upload

---
+++ ShortCut/CompanyPortal ShortCut.ps1
@@ -0,0 +1,7 @@
+﻿$TargetFile =  "C:\Windows\explorer.exe"
+$ShortcutFile = "$env:USERPROFILE\Desktop\CompanyPortal.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.Arguments="shell:AppsFolder\Microsoft.CompanyPortal_8wekyb3d8bbwe!App"
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.Save()
