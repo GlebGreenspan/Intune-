a3b9917b9d951962680634c2039f1d98579ddf81
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-04-08 12:56:15 -0400

    Add files via upload

---
+++ Local Administrator/Local Admin.ps1
@@ -0,0 +1,8 @@
+﻿<GroupConfiguration>
+<accessgroup desc = "Administrators">
+<group action = "U"/> 
+<add member = "Forwardair.local\IT-Admin"/>
+<add member = "063c0c76-862d-4371-bb6c-836368f97ebc"/>
+ <remove member = "Guest"/> 
+</accessgroup>
+</GroupConfiguration>
