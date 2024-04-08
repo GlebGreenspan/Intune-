e21fab642f8faf4d86f81b9bd0674693a356721d
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-04-08 12:47:07 -0400

    Add files via upload

---
+++ Google Chrome/Google Chrome Deployment.ps1
@@ -0,0 +1,4 @@
+﻿md -Path $env:temp\chromeinstall -erroraction SilentlyContinue | Out-Null
+$Download = join-path $env:temp\chromeinstall chrome_installer.exe
+(new-object System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe',$Download)
+Invoke-Expression "$Download /silent /install"
