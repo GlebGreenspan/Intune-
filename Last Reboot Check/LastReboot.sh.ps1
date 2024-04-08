2581a5cc4af99eb5b3ad12e36c7e8af6a498f0f4
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-04-08 12:49:22 -0400

    Add files via upload

---
+++ Last Reboot Check/LastReboot.sh.ps1
@@ -0,0 +1,2 @@
+﻿#Get all enabled servers that are in the domain.
+$Servers = Get-ADComputer -Filter {(OperatingSystem -like "*Server*") -and (Enabled -eq $true)} -Properties OperatingSystem | select -ExpandProperty Name | Sort-Object
