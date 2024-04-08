fee8342fe4dcd7689c588fbe0da9333398587366
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-04-08 11:16:41 -0400

    Add files via upload

---
+++ HP/Invoke-HPDriverUpdateRequirementRule.ps1
@@ -0,0 +1,12 @@
+﻿$Manufacturer = (Get-WmiObject -Class "Win32_ComputerSystem" | Select-Object -ExpandProperty Manufacturer).Trim()
+switch -Wildcard ($Manufacturer) {
+    "*HP*" {
+        Write-Output -InputObject "HP"
+    }
+    "*Hewlett-Packard*" {
+        Write-Output -InputObject "HP"
+    }
+    default {
+        Write-Output -InputObject $Manufacturer
+    }
+}
