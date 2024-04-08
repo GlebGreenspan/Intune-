5d1eabcbf1685fc2511052164ab3d33eb5bf3b57
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-04-08 12:37:36 -0400

    Add files via upload

---
+++ ShortCut/Shortcut for Teams.ps1
@@ -0,0 +1,10 @@
+﻿$SourceFileLocation = “$env:USERPROFILE\AppData\Local\Microsoft\teams\current\teams.exe”
+
+$ShortcutLocation = “$env:USERPROFILE\Desktop\teams.lnk”
+
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutLocation)
+
+$Shortcut.TargetPath = $SourceFileLocation
+
+$Shortcut.Save()
