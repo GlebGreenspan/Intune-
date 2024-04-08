6a646064010995350cae9ac0325658cfbd3a2d23
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-04-08 12:36:41 -0400

    Add files via upload

---
+++ ShortCut/Shortcut for Edge.ps1
@@ -0,0 +1,10 @@
+﻿$SourceFileLocation = “${Env:ProgramFiles(x86)}\Microsoft\Edge\Application\msedge.exe”
+
+$ShortcutLocation = “$env:USERPROFILE\Desktop\edge.lnk”
+
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutLocation)
+
+$Shortcut.TargetPath = $SourceFileLocation
+
+$Shortcut.Save()
