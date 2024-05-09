25bf6f3b4be107c008d69d5de877efe1ee7afbb9
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-05-09 17:29:03 -0400

    Update Clear-DownloadFolderRemediaton.ps1

--- Download Folder Clean Up/Clear-DownloadFolderRemediaton.ps1
+++ Download Folder Clean Up/Clear-DownloadFolderRemediaton.ps1
@@ -1,10 +1,3 @@
-<#
-Version: 1.0
-Author: 
-- Joey Verlinden (joeyverlinden.com)
-- Andrew Taylor (andrewstaylor.com)
-- Florian Slazmann (scloud.work)
-- Jannik Reinhard (jannikreinhard.com)
 Script: Get-TemplateRemediation
 Description:
 Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
