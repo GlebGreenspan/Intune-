4eb0705f77f016ad27a72b9075337bac99b4d8e8
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-05-09 17:59:39 -0400

    Update Detect_DeviceUptime7.ps1

--- Device Uptime and Reboot/Detect_DeviceUptime7.ps1
+++ Device Uptime and Reboot/Detect_DeviceUptime7.ps1
@@ -1,10 +1,3 @@
-<#
-Version: 1.0
-Author: 
-- Joey Verlinden (joeyverlinden.com)
-- Andrew Taylor (andrewstaylor.com)
-- Florian Slazmann (scloud.work)
-- Jannik Reinhard (jannikreinhard.com)
 Script: Get_DeviceUptime7
 Description: Checks the device uptime days. If its 7 days or more it shows a windows notification to the user that he should reboot.
 Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
