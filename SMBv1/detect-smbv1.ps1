42f6d4784d2c1fef25eebfa93d7690a1f30c8c15
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-05-09 17:34:45 -0400

    Update detect-smbv1.ps1

--- SMBv1/detect-smbv1.ps1
+++ SMBv1/detect-smbv1.ps1
@@ -1,10 +1,3 @@
-<#
-Version: 1.0
-Author: 
-- Joey Verlinden (joeyverlinden.com)
-- Andrew Taylor (andrewstaylor.com)
-- Florian Slazmann (scloud.work)
-- Jannik Reinhard (jannikreinhard.com)
 Script: detect-fastboot.ps1
 Description: Detects if SMBv1 is enabled
 Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
