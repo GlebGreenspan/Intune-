22fb09c9368b2e24e62513f92fca7b53d213cb91
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-05-09 17:56:47 -0400

    Update Remediate_CloudDeliveredProtection.ps1

--- Cloud Protection/Remediate_CloudDeliveredProtection.ps1
+++ Cloud Protection/Remediate_CloudDeliveredProtection.ps1
@@ -1,10 +1,3 @@
-﻿<#
-Version: 1.0
-Author: 
-- Joey Verlinden (joeyverlinden.com)
-- Andrew Taylor (andrewstaylor.com)
-- Florian Slazmann (scloud.work)
-- Jannik Reinhard (jannikreinhard.com)
 Script: Remediate-CloudDeliveredProtection
 Description:
 Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
