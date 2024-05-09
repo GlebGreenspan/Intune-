36fffa5c14b8b104355074ae5be3dfb38169df8e
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-05-09 17:25:10 -0400

    Add files via upload

---
+++ Microsoft Apps/Outlook/Clear-OutlookCacheRemedaiton.ps1
@@ -0,0 +1,16 @@
+<#
+Version: 1.0
+Author: 
+- Joey Verlinden (joeyverlinden.com)
+- Andrew Taylor (andrewstaylor.com)
+- Florian Slazmann (scloud.work)
+- Jannik Reinhard (jannikreinhard.com)
+Script: Clear-DownloadFolder
+Description:
+Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
+Version 1.0: Init
+Run as: Admin
+Context: 64 Bit
+#> 
+
+Start-Process -FilePath 'C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE' -ArgumentList '/cleanautocompletecache','/recycle'
