10c58794270f659a1d25519ce511fb5f9afe0ca7
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-05-09 17:41:19 -0400

    Update Detect_AdobeReader_Flash.ps1

--- Adobe Reader/Flash/Detect_AdobeReader_Flash.ps1
+++ Adobe Reader/Flash/Detect_AdobeReader_Flash.ps1
@@ -1,11 +1,4 @@
-﻿<#
-Version: 1.0
-Author: 
-- Joey Verlinden (joeyverlinden.com)
-- Andrew Taylor (andrewstaylor.com)
-- Florian Slazmann (scloud.work)
-- Jannik Reinhard (jannikreinhard.com)
-Script: Get-AdobeReader_Flash
+﻿Script: Get-AdobeReader_Flash
 Description:
 Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
 Version 1.0: Init
