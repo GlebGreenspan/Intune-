6d565a9e88d33accda93aff4b613b8ce37c73490
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-05-10 11:58:18 -0400

    Update installGoogleChrome.zsh

--- MacOS Apps/Google Chrome/installGoogleChrome.zsh
+++ MacOS Apps/Google Chrome/installGoogleChrome.zsh
@@ -21,7 +21,6 @@
 ## (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary
 ## loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility
 ## of such damages.
-## Feedback: neiljohn@microsoft.com
 
 # User Defined variables
 weburl="https://dl.google.com/chrome/mac/stable//googlechrome.dmg"              # What is the Azure Blob Storage URL?
