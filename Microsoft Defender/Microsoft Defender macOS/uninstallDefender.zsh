695f1053f3643ed3ab345072a573fd14f9574a08
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-05-10 11:47:54 -0400

    Update uninstallDefender.zsh

--- Microsoft Defender/Microsoft Defender macOS/uninstallDefender.zsh
+++ Microsoft Defender/Microsoft Defender macOS/uninstallDefender.zsh
@@ -15,7 +15,6 @@
 ## (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary
 ## loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility
 ## of such damages.
-## Feedback: neiljohn@microsoft.com
 
 # User Defined variables
 logandmetadir="/Library/Application Support/Microsoft/IntuneScripts/installDefender"      # The location of our logs and last updated data
