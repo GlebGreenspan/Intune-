9a01d078ce53ca879a3bfe28f622a79cde8e290e
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-05-10 16:22:49 -0400

    Add files via upload

---
+++ ShortCut/Custom URL shortcut/CustomURLshortcut.ps1
@@ -0,0 +1,7 @@
+﻿$new_object = New-Object -ComObject WScript.Shell
+$destination = $new_object.SpecialFolders.Item(“Desktop”)
+$source_path = Join-Path -Path $destination -ChildPath “\\afgroup.cloud.url”
+$source = $new_object.CreateShortcut($source_path)
+$source.TargetPath = 'https://afgroup.cloud.com'
+$Icon = 'C:\URLIcon\Citrix.ico'
+$source.Save()
