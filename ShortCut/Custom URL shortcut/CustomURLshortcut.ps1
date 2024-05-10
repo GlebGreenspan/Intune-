10f9a7ac786b972b3b4e02ccc5a4c69619c4f939
Gleb Greenspan <163175529+GlebGreenspan@users.noreply.github.com>
2024-05-10 16:24:19 -0400

    Update CustomURLshortcut.ps1

--- ShortCut/Custom URL shortcut/CustomURLshortcut.ps1
+++ ShortCut/Custom URL shortcut/CustomURLshortcut.ps1
@@ -1,7 +1,7 @@
 ﻿$new_object = New-Object -ComObject WScript.Shell
 $destination = $new_object.SpecialFolders.Item(“Desktop”)
-$source_path = Join-Path -Path $destination -ChildPath “\\afgroup.cloud.url”
+$source_path = Join-Path -Path $destination -ChildPath “\\NAME HERE .url”
 $source = $new_object.CreateShortcut($source_path)
-$source.TargetPath = 'https://afgroup.cloud.com'
-$Icon = 'C:\URLIcon\Citrix.ico'
+$source.TargetPath = 'https://google/com'
+$Icon = 'C:\URLIcon\google.ico'
 $source.Save()
